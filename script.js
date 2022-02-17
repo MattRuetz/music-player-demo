324dd4043da4868530d80e31d2756a5b872d3baa
MattRuetz <matt.b.ruetz@gmail.com>
2022-02-17 14:43:59 -0500

    buttons for prev/next/play are wired up, as well as time labels

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const image = document.querySelector('img');
 const title = document.getElementById('title');
 const artist = document.getElementById('artist');
 const music = document.querySelector('audio');
+const progressContainer = document.getElementById('progress-container');
+const progress = document.getElementById('progress');
+const currentTimeEl = document.getElementById('current-time');
+const durationEl = document.getElementById('duration');
 const prevBtn = document.getElementById('prev');
 const playBtn = document.getElementById('play');
 const nextBtn = document.getElementById('next');
@@ -64,16 +68,37 @@ let songIndex = 0;
 
 const nextSong = () => {
     songIndex++;
+    songIndex > songs.length-1 && (songIndex = 0)
     loadSong(songs[songIndex]);
     playSong();
 }
 
 const prevSong = () => {
     songIndex--;
+    songIndex < 0 && (songIndex = songs.length - 1)
     loadSong(songs[songIndex]);
     playSong();
+}
+
+const convertSecondsToTimeString = (timeInSecs) => {
+    return Math.floor(timeInSecs/60).toString() + ':' +
+        (timeInSecs%60<10 ? '0' + Math.floor(timeInSecs%60).toString() : Math.floor(timeInSecs%60).toString())
+};
+
+const updateProgressBar = (e) => {
+    isPlaying && ({duration, currentTime} = e.srcElement)
+    const progressPercent = (currentTime / duration) * 100;
+    progress.style.width = `${progressPercent}%`
+
+    if(duration){
+    durationEl.innerText = convertSecondsToTimeString(duration);
+    }
+    if(currentTime){
+    currentTimeEl.innerText = convertSecondsToTimeString(currentTime);
+    }
 }
 
 //Event Listeners
 prevBtn.addEventListener('click', prevSong);
 nextBtn.addEventListener('click', nextSong);
+music.addEventListener('timeupdate', updateProgressBar);
