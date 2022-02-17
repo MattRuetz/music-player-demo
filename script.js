7839d42a9c29bb9e79e7c2dfce9aed9908e999c3
MattRuetz <matt.b.ruetz@gmail.com>
2022-02-17 15:16:41 -0500

    styling to expand progress bar when user hovers over it. Also now wired up, so user can click to go to different point in song

--- script.js
+++ script.js
@@ -11,8 +11,7 @@ const playBtn = document.getElementById('play');
 const nextBtn = document.getElementById('next');
 
 //Music
-const songs = [
-    {
+const songs = [{
         name: 'jacinto-1',
         displayName: 'Electric Chill Machine',
         artist: 'Jacinto Design'
@@ -68,7 +67,7 @@ let songIndex = 0;
 
 const nextSong = () => {
     songIndex++;
-    songIndex > songs.length-1 && (songIndex = 0)
+    songIndex > songs.length - 1 && (songIndex = 0)
     loadSong(songs[songIndex]);
     playSong();
 }
@@ -81,24 +80,41 @@ const prevSong = () => {
 }
 
 const convertSecondsToTimeString = (timeInSecs) => {
-    return Math.floor(timeInSecs/60).toString() + ':' +
-        (timeInSecs%60<10 ? '0' + Math.floor(timeInSecs%60).toString() : Math.floor(timeInSecs%60).toString())
+    return Math.floor(timeInSecs / 60).toString() + ':' +
+        (timeInSecs % 60 < 10 ? '0' + Math.floor(timeInSecs % 60).toString() : Math.floor(timeInSecs % 60).toString())
 };
 
 const updateProgressBar = (e) => {
-    isPlaying && ({duration, currentTime} = e.srcElement)
-    const progressPercent = (currentTime / duration) * 100;
-    progress.style.width = `${progressPercent}%`
+        const {
+            duration,
+            currentTime
+        } = e.srcElement;
 
-    if(duration){
-    durationEl.innerText = convertSecondsToTimeString(duration);
-    }
-    if(currentTime){
-    currentTimeEl.innerText = convertSecondsToTimeString(currentTime);
-    }
+        const progressPercent = (currentTime / duration) * 100;
+        progress.style.width = `${progressPercent}%`
+
+        if (duration) {
+            durationEl.innerText = convertSecondsToTimeString(duration);
+        }
+        if (currentTime) {
+            currentTimeEl.innerText = convertSecondsToTimeString(currentTime);
+        }
+    
+}
+
+function setProgressBar(e) {
+    console.log(e)
+    const width = this.clientWidth;
+    const clickX = e.offsetX;
+    const {
+        duration,
+        currentTime
+    } = music;
+    music.currentTime = (clickX / width) * duration;
 }
 
 //Event Listeners
 prevBtn.addEventListener('click', prevSong);
 nextBtn.addEventListener('click', nextSong);
 music.addEventListener('timeupdate', updateProgressBar);
+progressContainer.addEventListener('click', setProgressBar);
